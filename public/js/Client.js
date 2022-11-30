f9d8f4db83c1e3a3b3edcb381044c0e3686f1e4a
enrell <enrellsa10@gmail.com>
2022-11-30 09:30:53 -0300

    implements vuejs

--- public/js/Client.js
+++ public/js/Client.js
@@ -1,56 +1,52 @@
-this.socket = io.connect(window.location.origin);
+import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+import { App } from '../../views/signup.ejs'
 
-const socket = null;
-const game = null;
-const playerName = null;
-const myTurn = null;
-const gameVerify = false;
-const turn = null;
-const message = "";
+const app = createApp(App, {
+  data() {
+    return {
+      message: '',
+      socket: null,
+      game: null,
+      playerName: null,
+      myTurn: null,
+      gameVerify: false,
+      turn: null,
 
-function start(){
-  this.message = "Wait oponnent";
-  this.gameVerify = true;
-  this.socket.emit("game.start", {
-  playerName: this.playerName,
-  });
-}
-function messageAlert(){
-  this.message = this.myTurn;
+    }
+  },
+  methods: {
+    start() {
+      this.message = "Wait oponnent";
+      this.gameVerify = true;
+      this.socket.emit("game.start", {
+      playerName: this.playerName,
+      });
+    },
+    messageAlert() {
+      this.message = this.myTurn;
 
-  if(this.message == this.myTurn){
-    console.log("Play!");
-  }else{
-    console.log("Wait oponnent play!");
+      if(this.message == this.myTurn){
+        console.log("Play!");
+      }else{
+        console.log("Wait oponnent play!");
+      }
+    },
+  },
+  mounted(){
+    this.socket = io.connect(window.location.origin);
+    const bckp = this;
+    this.socket.on("game.start", function(data){
+      bckp.game = data;
+      const myPlayer = data._player1._socketID;
+    
+      if (bckp.data.socket.id == myPlayer){
+        myPlayer = data._player1;
+      }else {
+        myPlayer = data._player2;
+      }
+      bckp.turn = myPlayer._turn;
+      bckp.myTurn = data._turnOf == bckp.turn;
+    });    
   }
-}
-const bckp = this;
-
-this.socket.on("game.start", function(data){
-  bckp.game = data;
-  const myPlayer = data._player1._socketID;
-
-  if (bckp.data.socket.id == myPlayer){
-    myPlayer = data._player1;
-  }else {
-    myPlayer = data._player2;
-  }
-  bckp.turn = myPlayer._turn;
-  bckp.myTurn = data._turnOf == bckp.turn;
-});
-
-// const squares = Array.from(document.querySelectorAll('.square'));
-
-// for (var i = 0; i < squares.length; i++) {
-//     squares[i].onclick = function(e){
-//         const x = '<img class="x" src="x.svg">';
-//         const o = '<img class="o" src="circle.svg">';
-
-//         if (turn == 0) {
-//           e.target.innerHTML = x;
-//         }
-//         if (turn == 1) {
-//           e.target.innerHTML = o;
-//         } 
-//     }
-// }
+})
+app.mount('#app')
